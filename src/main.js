535ef30e0d4798ba8ba703d8e6bdc8ab02dc033f
F0m1k23 <vitaliifomenko153@gmail.com>
2025-12-23 21:47:57 +0100

    v5

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router/index.js'
 import './style.css'
 
+// Initialize Telegram WebApp
+if (window.Telegram?.WebApp) {
+	window.Telegram.WebApp.ready()
+	window.Telegram.WebApp.expand()
+}
+
 createApp(App).use(router).mount('#app')
